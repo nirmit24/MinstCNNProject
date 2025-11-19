0023208375dc8dad85a84d0607b9fd7e2536161a
Nirmit Verma <72400288+nirmit24@users.noreply.github.com>
2025-11-19 01:56:42 +0000

    add initial file

---
+++ init_script.sh
@@ -0,0 +1,18 @@
+echo "A script to create , activate and install requirements.txt"
+echo "........."
+
+virtualenv venv
+
+echo "creation of virtualenv done....."
+echo " Activation my env"
+
+source venv/bin/activate
+
+echo "........."
+echo "install requirements.txt"
+
+pip install -r requirements.txt
+
+sleep(2)
+echo "install done"
+echo "Creation Activation and install of library done"
